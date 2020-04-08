ae1d05823cd811c221a7b7f5b19cb7d18a4518fc
Andrew <web@hu2.ru>
2020-04-08 13:32:30 +0300

    Update itb-config.js

--- itb-config.js
+++ itb-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   account: {
-    id: '6538b7e0f9d530fc17d6a00f946c2ec7',
-    token: '256d44c6b1c3da95e0578ba2af329393',
-    url: 'myshop-fz205.myinsales.ru',
+    id: '',
+    token: '',
+    url: '',
     http: true
   },
   theme: {
-    id: '1350396',
+    id: '',
     assetsSync: false,
     assetsDomain: 'https://assets.insales.ru'
   },
